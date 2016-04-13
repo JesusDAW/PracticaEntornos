34fc5f989ff8c16570d0f6c4d969eb8377e5e2fa
JesusDAW <JesusDAW@users.noreply.github.com>
2016-04-13 19:57:29 +0200

    PruebaGithub

---
+++ repaso1/Repaso1.java
@@ -0,0 +1,33 @@
+
+package repaso1;
+
+
+import java.util.*;
+public class Repaso1 {
+    
+    
+    public static void main(String[] args) {
+        int a=0;
+        int cincos=0;
+        int contador=0;
+        int acumulador=0;
+        Scanner sc=new Scanner(System.in);
+        
+        do{
+            System.out.print("Introduzca un numero: ");
+            a=sc.nextInt(); 
+            if (a==5){
+                cincos++;
+                }
+            contador++;
+            acumulador=acumulador + a;
+        }while(cincos<2); 
+        
+        System.out.println("El programa ha finalizado por que has introducido 2 veces el numero 5");
+       
+        System.out.println("La media arizmetica de la suma de los numeros introduciods es: "+ acumulador/contador);
+            
+        }
+        
+    }
+    
